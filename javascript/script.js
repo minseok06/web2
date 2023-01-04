659817717a6c07c19a58f8545c436ebba6a17c73
minseok06 <121544294+minseok06@users.noreply.github.com>
2023-01-04 18:29:25 +0900

    2022.01.04 PerpeECT

--- javascript/script.js
+++ javascript/script.js
@@ -9,4 +9,13 @@ jQuery(document).ready(function(){
 // $('.navi>li') navi의 <li>요소에 마우스오버와 마우스 아웃 이벤트
 // slideDown(500) jQuery에서 제공하는 함수로 슬라이딩 애니메이션을
 // 숫자 500은 0.5초를 의미
+$('.imgslide a:gt(0)').hide();
+setInterval(function(){
+    $('.imgslide a:first-child')
+    .fadeOut(1000)   /* 첫번쨰 요소를 선택하여 페이드아웃 실행 */
+    .next('a')   /* 다음 요소를 선택하여 페이드만 실행 */
+    .fadeIn(1000)
+    .end()   /* 이전요소 선택 */
+    .appendTo('.imgslide');
+},3000);
 });
